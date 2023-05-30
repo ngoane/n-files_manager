dfe9ba8aa6cef3753a7894d718df72d0ad0c061a
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-30 19:02:22 +0000

    Completed the utils for both redis and mongodb clients

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const dbUrl = `mongodb://${host}:${port}`;
+    this.connected = false;
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
